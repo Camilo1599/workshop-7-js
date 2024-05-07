2b6f4abef4ee936ed4beaae08175bb58bf151e30
alejoCas91 <alejocastrillon103@gmail.com>
2024-05-07 17:14:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+//¿Encuentras alguna redundancia en los atributos y metodos de las clases fuertes con respecto a las clases debiles?
+/*Clases debiles: oviparo y viviparo
+clases fuertes: animal
+los atributos redundantes pueden ser edad, nombre, estaVivo y genero
+y los metodos redundantes serian comer y dormir*/
